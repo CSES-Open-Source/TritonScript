06ea8f2d4379988e095ffa952dfcd368629b48c7
Aryen Singhal <aryensinghal@gmail.com>
2025-04-30 21:21:18 -0700

    fix merge conflict in Upload.tsx

--- frontend/src/pages/Upload.tsx
+++ frontend/src/pages/Upload.tsx
@@ -6,35 +6,6 @@ import uploadIcon from "../assets/upload-icon.png";
 export default function Upload() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    async function fetchUserNotes() {
-      if (!currentUser) return; // Ensure the user is logged in
-      try {
-        const res = await fetch(`${settings.domain}/api/notes?uploader=${currentUser.username}`);
-        const notes = await res.json();
-        setUserNotes(notes); // Update the state with fetched notes
-      } catch (error) {
-        console.error("Error fetching notes:", error);
-      }
-    }
-
-    fetchUserNotes();
-  }, [currentUser]);
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setFile(e.target.files[0]);
-    }
-  };
-
-  function handleChange(e: any) {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-  }
-  
-  async function handleSubmit(e: any) {
-    e.preventDefault();
-    const id = uuidv4();
-
   return (
     <div className="upload-container">
       <div className="upload-header">
